391e94e2b2dd431e1ccc5a1b2af218e4a79c11a2
Wang-PC <wang.t.h.f.g@gmail.com>
2018-12-05 07:55:54 +0800

    setting DB

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
-define('DB_DATABASE', '');
+define('DB_DATABASE', 'blog_0804');
 define('DB_CHARSET', 'utf8');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
